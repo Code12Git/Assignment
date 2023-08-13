88cde31dfde34ed56e583193c9e8095ee825a1f0
Code12Git <108590542+Code12Git@users.noreply.github.com>
2023-08-13 13:29:30 +0530

    Update axios.js

--- admin/src/utils/axios.js
+++ admin/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:7000/api",
+  baseURL: "https://schoolpen.onrender.com/api",
 });
 
 export default instance;
